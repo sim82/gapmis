af213b4ba1750cbf270dfc5d38ec660de912a815
Solon Pissis <solonas13@gmail.com>
2012-03-13 20:17:26 +0100

    libgapmis-0.0.7

--- errors.h
+++ errors.h
@@ -5,7 +5,14 @@
 #define         MATRIX		0x00000002
 #define         MAXGAP		0x00000004
 #define         MALLOC		0x00000008
+#define		NOGPU		0x00000016
 #define         BADCHAR		0x00000032
 #define         IO		0x00000064
+#define		GPUERROR	0x00000128
+#define		KERNEL		0x00000256
+#define		GPUMALLOC	0x00000512
 
 #endif
+
+
+
--- gapmis.h
+++ gapmis.h
@@ -1,6 +1,10 @@
 #ifndef         GAPMIS_H
 #define         GAPMIS_H
 
+#ifdef _USE_GPU
+#include "CL/opencl.h"
+#endif
+
 #define max(a,b) ((a) > (b)) ? (a) : (b)
 #define min(a,b) ((a) < (b)) ? (a) : (b)
 #define NUC_SCORING_MATRIX_SIZE         15		
@@ -56,5 +60,42 @@ static unsigned int opt_solution_scr ( int ** G, unsigned int n, unsigned int m,
 static unsigned int backtracing ( unsigned int ** H, unsigned int m, unsigned int n, unsigned int start, struct gapmis_align * out );
 static double total_scoring( unsigned int gap, double matrix_score, double gap_open_penalty, double gap_extend_penalty );
 static unsigned int num_mismatch ( const char * seqa, unsigned int seqa_len, const char * seqb, unsigned int seqb_len, struct gapmis_align * out );
+
+
+#ifdef _USE_GPU
+/* Computes the optimal semi-global alignment with the maximum score between a pattern p and all texts t */
+unsigned int gapmis_one_to_many_opt_gpu ( const char * p, const char const ** t, const struct gapmis_params * in, struct gapmis_align * out );
+
+/* Computes the optimal semi-global alignment with the maximum score between each pattern p and all texts t */
+unsigned int gapmis_many_to_many_opt_gpu (const char const ** p, const char const ** t, const struct gapmis_params * in, struct gapmis_align * out );
+
+static cl_platform_id * get_gpu_id(int * error);
+static cl_device_id * get_dev_id(cl_platform_id * gpu_id, int * error);
+static cl_context create_context(cl_device_id * dev_id, int * error);
+static cl_command_queue create_cmd_queue (cl_device_id * dev_id, cl_context context, int * error);
+static cl_kernel load_kernel (char * name, char * kernel_name, cl_device_id * dev_id, cl_context context, int * error);
+static cl_mem malloc_device (cl_context context, size_t size, int * error);
+static void init_device_mem_int (cl_context context, cl_command_queue cmd_queue, cl_mem dev_mem, int * mem, size_t size, int * error);
+static void init_device_mem_uint (cl_context context, cl_command_queue cmd_queue, cl_mem dev_mem, unsigned int * mem, size_t size, int * error);
+static void init_device_mem_float (cl_context context, cl_command_queue cmd_queue, cl_mem dev_mem, float * mem, size_t size, int * error);
+static void set_kernel_arguments (cl_kernel kernel, cl_command_queue cmd_queue, cl_mem cl_mem0, cl_mem cl_mem1, cl_mem cl_mem2, cl_mem cl_mem3, cl_mem cl_mem4, cl_mem cl_mem5, cl_mem cl_mem6, cl_mem cl_mem7);
+static void read_device_mem_float (cl_command_queue cmd_queue, size_t size, float * mem, cl_mem dev_mem, int * error);
+static void fill_txtsLenVec (unsigned int totalTxts, const char const ** in, int * outVec);
+static void fill_argsVec (unsigned int totalPats, unsigned int totalTxts, const char const ** in, unsigned int max_gap, unsigned int pBlockSize, unsigned int maxPatLen, unsigned int maxTxtLen, int * outVec);
+static void fill_patsVec (unsigned int total, unsigned int blockSize, const char const ** in, unsigned int * outVec, int matrix);
+static void fill_txtsVec (unsigned int total, unsigned int blockSize, const char const ** in, unsigned int * outVec, int matrix);
+static unsigned int get_pblock_size (unsigned int input, unsigned int mult);
+static unsigned int get_max_length (unsigned int total, const char ** input);
+static unsigned int get_min_length (unsigned int total, const char ** input);
+static unsigned int get_number_of_sequences (const char ** input);
+static unsigned int get_number_of_groups (int elements, int groupSize);
+static unsigned int kernel_launch (cl_kernel kernel, cl_context context, cl_command_queue cmd_queue, const char const ** p, const char const ** t, const struct gapmis_params * in, float * scores);
+static unsigned int kernel_launch_l (cl_kernel kernel, cl_context context, cl_command_queue cmd_queue, const char const ** p, const char const ** t, const struct gapmis_params * in, float * scores, struct gapmis_align * out);
+static void update_group_match (float * groupScores, int * groupMatch, float * groupMatchScores, unsigned int patGroupSize, unsigned int txtGroupSize, int pats, int txts, int patGroupIndex, int txtGroupIndex);
+static void set_invalid(int * groupMatch, int groupSize);
+static void set_minimum(float * groupMatchScores, int groupSize);
+static void set_null (const char ** input, int size);
+static void initialize_pointers (const char * groupPatterns[], int groupIndex, int groupSize, const char const ** source, int sourceSize);
+#endif
 
 #endif
